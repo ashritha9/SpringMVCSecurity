c7f6fd1eccd680695f958a01d9c96871dd85efad
Bhargavi <bhargaviprasadula@gmail.com>
2026-07-21 22:53:42 +0530

    SpringMVCSecurity-DefaultFilterChainWithRoles

---
+++ src/main/java/com/practice/demoSecurityMVC/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.practice.demoSecurityMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String showHome()
+	{
+		return "home";
+	}
+	
+}
---
+++ src/test/java/com/practice/demoSecurityMVC/DemoSecurityMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.demoSecurityMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
